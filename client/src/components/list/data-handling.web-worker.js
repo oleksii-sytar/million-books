4d2322188bacb5676397309fd883c0460f635b10
Oleksii Sytar <oleksii.sytar@gmail.com>
2020-03-25 01:21:07 +0200

    fetch

--- client/src/components/list/data-handling.web-worker.js
+++ client/src/components/list/data-handling.web-worker.js
@@ -1,54 +1,8 @@
 const workercode = () => {
-  const xhr = new XMLHttpRequest();
-  const BooksDB = indexedDB.open('BooksDB', 1);
-  let db = null;
-  let booksList = [];
+  onmessage = async e => {
+    const res = await fetch('/api/books');
 
-  xhr.open('GET', 'http://localhost:3000/api/books');
-
-  xhr.onload = () => {
-    booksList = JSON.parse(xhr.response);
-
-    postMessage(booksList);
-  };
-
-  BooksDB.onupgradeneeded = (event) => {
-    const db = event.target.result;
-
-    if (!db.objectStoreNames.contains('books')) {
-      const booksOS = db.createObjectStore('books', { keyPath: '_id' });
-      booksOS.createIndex('_id', '_id', { unique: true });
-      // const filteredBooksOS = db.createObjectStore('filtered', { keyPath: '_id' });
-      // filteredBooksOS.createIndex('_id', '_id', { unique: true });
-    }
-  };
-
-  BooksDB.onsuccess = (event) => {
-    db = event.target.result;
-  }
-
-  const filterBooks = searchQuery => {
-    const objectStore = db.transaction(['books'], 'readwrite').objectStore('books');
-    booksList.forEach(book => objectStore.add(book));
-
-    // booksList.forEach(book => {
-    //   if (
-    //     book.name.includes(searchQuery) ||
-    //     book.author.name.includes(searchQuery) ||
-    //     book.author.gender.includes(searchQuery) ||
-    //     book.genre.includes(searchQuery)
-    //   ) {
-    //     // console.log(book._id);
-    //   }
-    // });
-  }
-
-  onmessage = e => {
-    if (!booksList.length) {
-      xhr.send();
-    }
-    
-    filterBooks(e.data)
+    postMessage(res);
   }
 };
 
